ce6c202d65f6dd4da5400308e0d8f42a3fbc226c
lynseyv <lynseyv@gmail.com>
2014-01-28 11:05:27 -0500

    Final Project

---
+++ _Final Project/js/main.js
@@ -0,0 +1,313 @@
+// J
+
+// $('.vid-box').hover(function() {
+// $(this).toggleClass('colors');
+// });
+
+// $(document).ready(function(){
+//  $(".vid-box").hover(function() {
+//     $(this).stop().animate({opacity: "0.8"}, 'slow');
+//    },
+//    function() {
+//      $(this).stop().animate({opacity: "1"}, 'slow');
+//    });
+//});
+
+//VIDEO 1
+$(document).ready( function() {
+
+$('#vid-1').click( function() {            
+	loadPopupBoxOne();
+});
+        
+$('#popupBoxCloseOne').click( function() {
+	unloadPopupBoxOne();
+});
+
+function unloadPopupBoxOne() {    // TO Unload the Popupbox
+	$('#popup-box-1').fadeOut("slow");
+	$("#vid-1").css({ // this is just for style        
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxOne() {    // To Load the Popupbox
+	$('#popup-box-1').fadeIn("slow");
+    $("#vid-1").css({ // this is just for style
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 2
+$(document).ready( function() {
+
+$('#vid-2').click( function() {            
+	loadPopupBoxTwo();
+});
+        
+$('#popupBoxCloseTwo').click( function() {
+	unloadPopupBoxTwo();
+});
+
+function unloadPopupBoxTwo() {    // TO Unload the Popupbox
+	$('#popup-box-2').fadeOut("slow");
+	$("#vid-2").css({ // this is just for style        
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxTwo() {    // To Load the Popupbox
+	$('#popup-box-2').fadeIn("slow");
+    $("#vid-2").css({ // this is just for style
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 3
+$(document).ready( function() {
+
+$('#vid-3').click( function() {            
+	loadPopupBoxThree();
+});
+        
+$('#popupBoxCloseThree').click( function() {
+	unloadPopupBoxThree();
+});
+
+function unloadPopupBoxThree() {    // TO Unload the Popupbox
+	$('#popup-box-3').fadeOut("slow");
+	$("#vid-3").css({ // this is just for style        
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxThree() {    // To Load the Popupbox
+	$('#popup-box-3').fadeIn("slow");
+    $("#vid-3").css({ // this is just for style
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 4
+$(document).ready( function() {
+
+$('#vid-4').click( function() {            
+	loadPopupBoxFour();
+});
+        
+$('#popupBoxCloseFour').click( function() {
+	unloadPopupBoxFour();
+});
+
+function unloadPopupBoxFour() {    // TO Unload the Popupbox
+	$('#popup-box-4').fadeOut("slow");
+	$("#vid-4").css({ // this is just for style        
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxFour() {    // To Load the Popupbox
+	$('#popup-box-4').fadeIn("slow");
+    $("#vid-4").css({ // this is just for style
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 5
+$(document).ready( function() {
+
+$('#vid-5').click( function() {            
+	loadPopupBoxFive();
+});
+        
+$('#popupBoxCloseFive').click( function() {
+	unloadPopupBoxFive();
+});
+
+function unloadPopupBoxFive() {    // TO Unload the Popupbox
+	$('#popup-box-5').fadeOut("slow");
+	$("#vid-5").css({ // this is just for style        
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxFive() {    // To Load the Popupbox
+	$('#popup-box-5').fadeIn("slow");
+    $("#vid-5").css({ // this is just for style
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 6
+$(document).ready( function() {
+
+$('#vid-6').click( function() {            
+	loadPopupBoxSix();
+});
+        
+$('#popupBoxCloseSix').click( function() {
+	unloadPopupBoxSix();
+});
+
+function unloadPopupBoxSix() {    // TO Unload the Popupbox
+	$('#popup-box-6').fadeOut("slow");
+	$("#vid-6").css({ // this is just for style        
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxSix() {    // To Load the Popupbox
+	$('#popup-box-6').fadeIn("slow");
+    $("#vid-6").css({ // this is just for style
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 7
+$(document).ready( function() {
+
+$('#vid-7').click( function() {            
+	loadPopupBoxSeven();
+});
+        
+$('#popupBoxCloseSeven').click( function() {
+	unloadPopupBoxSeven();
+});
+
+function unloadPopupBoxSeven() {   
+	$('#popup-box-7').fadeOut("slow");
+	$("#vid-7").css({    
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxSeven() {    
+	$('#popup-box-7').fadeIn("slow");
+    $("#vid-7").css({ 
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 8
+$(document).ready( function() {
+
+$('#vid-8').click( function() {            
+	loadPopupBoxEight();
+});
+        
+$('#popupBoxCloseEight').click( function() {
+	unloadPopupBoxEight();
+});
+
+function unloadPopupBoxEight() {   
+	$('#popup-box-8').fadeOut("slow");
+	$("#vid-8").css({    
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxEight() {    
+	$('#popup-box-8').fadeIn("slow");
+    $("#vid-8").css({ 
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 9
+$(document).ready( function() {
+
+$('#vid-9').click( function() {            
+	loadPopupBoxNine();
+});
+        
+$('#popupBoxCloseNine').click( function() {
+	unloadPopupBoxNine();
+});
+
+function unloadPopupBoxNine() {   
+	$('#popup-box-9').fadeOut("slow");
+	$("#vid-9").css({    
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxNine() {    
+	$('#popup-box-9').fadeIn("slow");
+    $("#vid-9").css({ 
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 10
+$(document).ready( function() {
+
+$('#vid-10').click( function() {            
+	loadPopupBoxTen();
+});
+        
+$('#popupBoxCloseTen').click( function() {
+	unloadPopupBoxTen();
+});
+
+function unloadPopupBoxTen() {   
+	$('#popup-box-10').fadeOut("slow");
+	$("#vid-10").css({    
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxTen() {    
+	$('#popup-box-10').fadeIn("slow");
+    $("#vid-10").css({ 
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+//VIDEO 11
+$(document).ready( function() {
+
+$('#vid-11').click( function() {            
+	loadPopupBoxEleven();
+});
+        
+$('#popupBoxCloseEleven').click( function() {
+	unloadPopupBoxEleven();
+});
+
+function unloadPopupBoxEleven() {   
+	$('#popup-box-11').fadeOut("slow");
+	$("#vid-11").css({    
+		"opacity": "1"  
+			}); 
+}    
+        
+function loadPopupBoxEleven() {    
+	$('#popup-box-11').fadeIn("slow");
+    $("#vid-11").css({ 
+        "opacity": "0.3"  
+            });         
+        }        
+    });
+
+
+
